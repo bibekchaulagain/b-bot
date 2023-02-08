78965d9555e740382a7853b5f8e8bd6a4762c3b5
bibekchaulagain <124770426+bibekchaulagain@users.noreply.github.com>
2023-02-08 17:30:33 +0900

    Update b_bot.py

--- b_bot.py
+++ b_bot.py
@@ -34,7 +34,6 @@ if user_input:
     st.session_state.generated.append(output)
 
 if st.session_state["generated"]:
-    print(st.session_state)
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")  # NoQA
